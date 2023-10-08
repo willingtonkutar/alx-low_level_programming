45f3fba11eefe6a35be91ccea18a7e4885c690c6
willingtonkutar <willykutar@gmail.com>
2023-10-08 09:41:04 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - what is supposed to be main
+ *
+ * Return: 0 when it all goes well
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
