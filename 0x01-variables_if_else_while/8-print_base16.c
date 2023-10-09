7fc0e3a96d4eb542e56782583786fbebce1ef851
willingtonkutar <willykutar@gmail.com>
2023-10-09 13:53:52 +0000

    push

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - always main
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	char m;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (m = 'a' ; m <= 'f' ; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
