343729db4822b108d51a4778cb8cde3ea880bca5
willingtonkutar <willykutar@gmail.com>
2023-10-12 12:14:31 +0000

    task completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - lower case letters
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char m;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (m = 'a' ; m <= 'z' ; m++)
+			_putchar(m);
+	}
+	_putchar('\n');
+}
