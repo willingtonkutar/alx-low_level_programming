54045442ca3e5b3de027b92459029bde71883893
willingtonkutar <willykutar@gmail.com>
2023-10-13 17:16:00 +0000

    task finished

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - used to multiply two integers
+ *@a: parameter one
+ *@b: parameter two
+ *Return: answer gotten
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
