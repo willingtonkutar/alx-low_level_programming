0f1009559b4efeef1e58b6ace4e35a5682966c46
willingtonkutar <willykutar@gmail.com>
2023-10-18 02:58:00 +0000

    currently finished task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	    int i;
+	        char password[11];
+
+		    srand(time(NULL));
+
+		        for (i = 0; i < 10; i++) {
+				        password[i] = rand() % 94 + 33;
+					    }
+			    password[10] = '\0';
+
+			        printf("%s\n", password);
+
+				    return (0);
+}
