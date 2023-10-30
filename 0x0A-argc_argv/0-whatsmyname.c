7385672e3ca888189f7d8f9db9de1c6467ed36e7
willingtonkutar <willykutar@gmail.com>
2023-10-30 20:58:19 +0000

    task finished

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - print the programname
+ *@argc: argument counted
+ *@argv: vector for argument
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
