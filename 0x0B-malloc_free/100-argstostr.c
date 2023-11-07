b5c5e61d3f3d08c41efad47daa603db76c073502
willingtonkutar <willykutar@gmail.com>
2023-11-07 15:13:49 +0000

    finished task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *argstostr - concatenates arguments used in a function
+ *@ac: counts arguments
+ *@av: arguments vector
+ *Return: pointer to a concatenated string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0;
+	int count = 0;
+	char *output;
+
+	for (i = 0 ; i < ac ; i++)
+	{
+		for (j = 0 ; av[i][j] != '\0' ; j++)
+			count++;
+		count++;
+	}
+	output = malloc(sizeof(char) * count + 1);
+	if (output == NULL)
+		return (NULL);
+
+	for (i = 0 ; av[i][j] != '\0' ; j++)
+	{
+		for (j = 0 ; av[i][j] != '\0' ; j++)
+		{
+			output[k] = av[i][j];
+			k++;
+		}
+		output[k++] = '\n';
+	}
+	output[k] = '\0';
+	return (output);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *free_grid - free memory previously allocated
+ *@grid: pointer to the array
+ *@height: number of rows used
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0 ; i < height ; i++)
+		free(grid[i]);
+	free(grid);
+}
