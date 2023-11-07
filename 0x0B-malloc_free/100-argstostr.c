faea1728f4c8f71eb2e414c97034f7388c547f32
willingtonkutar <willykutar@gmail.com>
2023-11-07 15:20:57 +0000

    finished task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,7 +23,7 @@ char *argstostr(int ac, char **av)
 	if (output == NULL)
 		return (NULL);
 
-	for (i = 0 ; av[i][j] != '\0' ; j++)
+	for (i = 0 ; i < ac ; i++)
 	{
 		for (j = 0 ; av[i][j] != '\0' ; j++)
 		{
