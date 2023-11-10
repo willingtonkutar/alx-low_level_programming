343252de2cfbd9e343ed10c6247b59f6af5ba0f7
willingtonkutar <willykutar@gmail.com>
2023-11-10 11:45:29 +0000

    finished task ready for checker

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,24 +3,41 @@
 #include "3-calc.h"
 #include <stddef.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - prints result of a simple operation
+ * @argc: number of programs supplied to program
+ * @argv: array of pointers to arguments
+ * Return: always 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
+	int num1, num2;
+	char *op;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-	char *operator = argv[2];
-	int result;
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	if ((*op == '/' && num2 == 0) ||
+		(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 
-	int (*operation)(int, int) = get_op_func(operator);
-
-	result = operation(num1, num2);
-
-	printf("%d\n", result);
 	return (0);
 }
